cefa73c3dd5aaf080321d5204ebe403b29b13195
Hetvi Sardhara <your@email.com>
2026-05-25 01:29:02 +0530

    fix brandidentity import case

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Loader from "../components/Loader";
 import Navbar from "../components/Navbar";
 import { motion } from "framer-motion";
 import fmcg from "../assets/fmcg.jpeg";
-import brandidentity from "../assets/brandidentity.jpeg";
+import brandidentity from "../assets/brandidentity.JPG";
 import giftwrap from "../assets/giftwrap.jpeg";
 
 function Home() {
